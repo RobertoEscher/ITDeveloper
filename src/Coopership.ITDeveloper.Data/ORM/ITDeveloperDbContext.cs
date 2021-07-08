60107564794673d097539d2b3810742dc1a0c693
RobertoEscher <robertoescher@hotmail.com>
2021-07-08 18:26:17 -0300

    Migration

---
+++ src/Coopership.ITDeveloper.Data/ORM/ITDeveloperDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Coopership.ITDeveloper.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coopership.ITDeveloper.Data.ORM
+{
+    public class ITDeveloperDbContext : DbContext
+    {
+         
+        public ITDeveloperDbContext(DbContextOptions<ITDeveloperDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Mural> Mural { get; set; }
+    }
+}
--- src/Coopership.ITDeveloper.Mvc/Startup.cs
+++ src/Coopership.ITDeveloper.Mvc/Startup.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Coopership.ITDeveloper.Data.ORM;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Coopership.ITDeveloper.Mvc
 {
@@ -31,7 +28,7 @@ namespace Coopership.ITDeveloper.Mvc
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddDbContext<ITDeveloperDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultITDeveloper")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
