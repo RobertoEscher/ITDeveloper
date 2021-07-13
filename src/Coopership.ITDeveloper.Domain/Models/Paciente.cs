c58a67f04fcd32652be6a4eee8f0a5d59f200f56
RobertoEscher <robertoescher@hotmail.com>
2021-07-13 14:39:54 -0300

    Custom Tag Helper - Complemento

--- src/Coopership.ITDeveloper.Domain/Models/Paciente.cs
+++ src/Coopership.ITDeveloper.Domain/Models/Paciente.cs
@@ -1,6 +1,7 @@
 ﻿using Coopership.ITDeveloper.Domain.Entities;
 using Coopership.ITDeveloper.Domain.Enums;
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -17,6 +18,8 @@ namespace Coopership.ITDeveloper.Domain.Models
         [Display(Name = "Estado do Paciente")]
         public Guid EstadoPacienteId { get; set; }
         public virtual EstadoPaciente EstadoPaciente { get; set; }
+
+        [DisplayName(displayName:"Nome do Paciente")]
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
         public DateTime DataInternacao { get; set; }
