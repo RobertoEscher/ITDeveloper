47ecda6bb90811e01c7f645621e6263110d800ab
RobertoEscher <robertoescher@hotmail.com>
2021-07-13 10:29:17 -0300

    Criando View Component - Cabeçalho

---
+++ src/Coopership.ITDeveloper.Mvc/ViewComponents/CabecalhoModulos/CabecalhoModulosViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using Coopership.ITDeveloper.Mvc.ViewComponents.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Coopership.ITDeveloper.Mvc.ViewComponents.CabecalhoModulos
+{
+    [ViewComponent(Name = "Cabecalho")]
+    public class CabecalhoModulosViewComponents : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string titulo, string subtitulo)
+        {
+            var modulo = new Modulo()
+            {
+                Titulo = titulo,
+                Subtitulo = subtitulo
+            };
+
+            return View(modulo);
+        }
+    }
+}
---
+++ src/Coopership.ITDeveloper.Mvc/ViewComponents/Helpers/Modulo.cs
@@ -0,0 +1,8 @@
+﻿namespace Coopership.ITDeveloper.Mvc.ViewComponents.Helpers
+{
+    public class Modulo
+    {
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+    }
+}
