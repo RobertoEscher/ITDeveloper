8e68f32292e6712a9da01409c6ba80f2879d6402
RobertoEscher <robertoescher@hotmail.com>
2021-07-26 17:01:00 -0300

    Interface IUserInContext

--- src/Cooperchip.ITDeveloper.Mvc/Configuration/DependencyInjectConfig.cs
+++ src/Cooperchip.ITDeveloper.Mvc/Configuration/DependencyInjectConfig.cs
@@ -1,11 +1,16 @@
-﻿using Cooperchip.ITDeveloper.Mvc.Extensions.Filters;
+﻿using Cooperchip.ITDeveloper.Domain.Interfaces;
+using Cooperchip.ITDeveloper.Mvc.Extensions.Filters;
+using Cooperchip.ITDeveloper.Mvc.Extensions.Identity;
 using Cooperchip.ITDeveloper.Mvc.Extensions.Identity.Services;
 using Cooperchip.ITDeveloper.Mvc.Infra;
+using Coopership.ITDeveloper.CrossCutting.Helpers;
 using KissLog;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Cooperchip.ITDeveloper.Mvc.Configuration
 {
@@ -15,6 +20,13 @@ namespace Cooperchip.ITDeveloper.Mvc.Configuration
         {
             services.AddTransient<IUnitOfUpload, UnitOfUpload>();
 
+            // == Mantem o contexto http por toda a aplicação
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // =====
+            services.AddScoped<IUserInContext<Guid>, AspNetUser>();
+
+            services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>, UserClaimsService>();
+            
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped((context) => Logger.Factory.Get());
             services.AddScoped<AuditoriaIloggerFilter>();
